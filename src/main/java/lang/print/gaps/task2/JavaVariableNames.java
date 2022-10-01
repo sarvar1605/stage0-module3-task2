764cacff9bacef15571101a3869750da169fc31f
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-01 16:18:17 +0500

    Update JavaVariableNames.java

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int tomorrow = 0;
-        int $Money = 10;
+        int dollarMoney = 10;
         int percentHundred = 100;
         int exclamationBack = 1000;
         int _underScore = 10000;
